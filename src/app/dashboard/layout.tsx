926a18267651ce5ac2fe530253a5bab3b499420b
Trevor Mugo <trevormugolawrence@gmail.com>
2025-07-14 19:12:22 +0000

    now there is too much space below it, lol

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,4 +1,3 @@
-
 "use client"
 import type { PropsWithChildren } from 'react'
 import { SidebarNav } from '@/components/dashboard/sidebar-nav'
@@ -9,8 +8,10 @@ export default function DashboardLayout({ children }: PropsWithChildren) {
       <div className="flex h-screen w-full flex-col bg-muted/40">
         <SidebarNav />
         <div className="flex flex-col sm:pl-14 h-full overflow-hidden">
-          <Header />
-          <main className="flex-1 overflow-y-auto p-4 sm:p-6">
+          <header className="sm:pt-6">
+            <Header />
+          </header>
+          <main className="flex-1 overflow-y-auto p-4 sm:p-6 sm:pt-4">
             <div className="grid gap-4 md:gap-8">
                 {children}
             </div>
