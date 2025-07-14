2079b7d6e9f2961eaf10a19799c177934e5a1a60
Trevor Mugo <trevormugolawrence@gmail.com>
2025-07-14 21:07:56 +0000

    the appbar , I mean where the role is writtern ans the current page, it

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -8,9 +8,9 @@ export default function DashboardLayout({ children }: PropsWithChildren) {
   return (
       <div className="flex h-screen bg-muted/40 overflow-hidden">
         <SidebarNav />
-        <div className="flex flex-col flex-1">
+        <div className="flex flex-col flex-1 sm:pl-14">
           <Header />
-          <main className="flex-1 overflow-y-auto p-4 sm:p-6 sm:pl-20">
+          <main className="flex-1 overflow-y-auto p-4 sm:p-6">
             {children}
           </main>
         </div>
